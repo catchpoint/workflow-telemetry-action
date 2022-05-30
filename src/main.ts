9136e232b0daa498d3a2442ca812ea7fd18832b0
Serkan ÖZAL <serkan@thundra.io>
2022-05-30 09:58:53 +0300

    Fix `main.ts`

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
 import * as core from '@actions/core'
 import path from 'path'
+import { ChildProcess, spawn } from "child_process"
 
 async function run(): Promise<void> {
   try {
     core.info(`[WM] Initialing ...`)
 
-    const spawn = require('child_process').spawn
-    const child = spawn(process.argv[0], [path.join(__dirname, '../sc/index.js')], {
-      detached: true,
-      stdio: 'inherit'
-    })
+    const child: ChildProcess =
+        spawn(
+          process.argv[0],
+          [path.join(__dirname, '../sc/index.js')],
+          {
+            detached: true,
+            stdio: 'ignore'
+          })
     child.unref()
 
-    core.info(`[WM] Initialing completed`)
+    core.info(`[WM] Initialization completed`)
   } catch (error: any) {
     core.setFailed(error.message)
   }
