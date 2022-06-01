e1d36c1cbbb0c62112aff6ceacfd3653adc8128a
Serkan ÖZAL <serkan@thundra.io>
2022-06-01 12:31:42 +0300

    Refactor `statCollector.ts` and add disk I/O stat support

--- src/statCollector.ts
+++ src/statCollector.ts
@@ -1,7 +1,15 @@
 import si from 'systeminformation'
 import { createServer, IncomingMessage, Server, ServerResponse } from 'http'
+import * as logger from './logger'
 
 const STATS_FREQ: number = 5000
+const HOST: string = 'localhost'
+// TODO
+// It is better to find an available port and use it.
+// The post script (post.ts) needs to know the selected port
+const PORT: number = 7777
+
+let expectedScheduleTime: number = 0
 let statCollectTime: number = 0
 
 // Network Stats         //
@@ -9,8 +17,8 @@ let statCollectTime: number = 0
 
 interface NetworkStats {
   readonly time: number
-  readonly rxKb: number
-  readonly txKb: number
+  readonly rxMb: number
+  readonly txMb: number
 }
 
 const networkStatsHistogram: NetworkStats[] = []
@@ -26,45 +34,84 @@ function collectNetworkStats(statTime: number, timeInterval: number) {
       }
       const networkStats: NetworkStats = {
         time: statTime,
-        rxKb: Math.floor((totalRxSec * (timeInterval / 1000)) / 1024),
-        txKb: Math.floor((totalTxSec * (timeInterval / 1000)) / 1024)
+        rxMb: Math.floor((totalRxSec * (timeInterval / 1000)) / 1024 / 1024),
+        txMb: Math.floor((totalTxSec * (timeInterval / 1000)) / 1024 / 1024)
       }
       networkStatsHistogram.push(networkStats)
     })
     .catch((error: any) => {
-      console.error(error)
+      logger.error(error)
+    })
+}
+
+///////////////////////////
+
+// Disk Stats            //
+///////////////////////////
+
+const diskStatsHistogram: DiskStats[] = []
+
+interface DiskStats {
+  readonly time: number
+  readonly rxMb: number
+  readonly wxMb: number
+}
+
+function collectDiskStats(statTime: number, timeInterval: number) {
+  si.fsStats()
+    .then((data: si.Systeminformation.FsStatsData) => {
+      let rxSec = data.rx_sec ? data.rx_sec : 0;
+      let wxSec = data.wx_sec ? data.wx_sec : 0;
+      const diskStats: DiskStats = {
+        time: statTime,
+        rxMb: Math.floor((rxSec * (timeInterval / 1000)) / 1024 / 1024),
+        wxMb: Math.floor((wxSec * (timeInterval / 1000)) / 1024 / 1024)
+      }
+      diskStatsHistogram.push(diskStats)
+    })
+    .catch((error: any) => {
+      logger.error(error)
     })
 }
 
 ///////////////////////////
 
 function collectStats() {
-  if (!statCollectTime) {
-    const currentTime: number = Date.now()
-    const timeSec: number = currentTime - (currentTime % 1000)
-    statCollectTime = timeSec
-  } else {
-    statCollectTime += STATS_FREQ
+  try {
+    if (!statCollectTime) {
+      const currentTime: number = Date.now()
+      const timeSec: number = currentTime - (currentTime % 1000)
+      statCollectTime = timeSec
+    } else {
+      statCollectTime += STATS_FREQ
+    }
+    collectNetworkStats(statCollectTime, STATS_FREQ)
+    collectDiskStats(statCollectTime, STATS_FREQ)
+  } finally {
+    expectedScheduleTime += STATS_FREQ;
+    setTimeout(collectStats, expectedScheduleTime - Date.now());
   }
-  collectNetworkStats(statCollectTime, STATS_FREQ)
 }
 
-console.log('[WM] Starting stat collector ...')
-
-setInterval(collectStats, STATS_FREQ)
-
 function startHttpServer() {
-  const HOST: string = 'localhost'
-  const PORT: number = 7777
-
   const server: Server = createServer(
     (request: IncomingMessage, response: ServerResponse) => {
       switch (request.url) {
         case '/network': {
-          console.log('[WM] Received network request')
           if (request.method === 'GET') {
-            console.log('[WM] Received network get request')
             response.end(JSON.stringify(networkStatsHistogram))
+          } else {
+            response.statusCode = 405
+            response.end()
+          }
+          break
+        }
+        case '/disk': {
+          if (request.method === 'GET') {
+            response.end(JSON.stringify(diskStatsHistogram))
+          } else {
+            response.statusCode = 405
+            response.end()
           }
           break
         }
@@ -77,20 +124,20 @@ function startHttpServer() {
   )
 
   server.listen(PORT, HOST, () => {
-    console.log(`[WM] Stat server listening on port ${PORT}`)
+    logger.info(`Stat server listening on port ${PORT}`)
   })
 }
 
-console.log('[WM] Starting HTTP server ...')
+function init() {
+  expectedScheduleTime = Date.now() + STATS_FREQ;
 
-startHttpServer()
+  logger.info('Starting stat collector ...')
+  setTimeout(collectStats, STATS_FREQ);
 
-/*
-setInterval(() => {
-    core.info('>>>>> Dumping network stats ...')
-    for (let [time, stats] of networkStatsHistogram.entries()) {
-        const timeStr: string = new Date(time).toISOString()
-        core.info(`>>>>> Time: ${timeStr}, stats: ${JSON.stringify(stats)}`)
-    }
-}, 10000)
-*/
+  logger.info('Starting HTTP server ...')
+  startHttpServer()
+}
+
+const currentTime = Date.now();
+const nextSec = currentTime + 1000 - (currentTime % 1000);
+setTimeout(init, nextSec - currentTime - 1);
