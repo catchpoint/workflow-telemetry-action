fddfdebb092cc6ffd453d1de4992569a03946385
Tiago Pascoal <tspascoal@github.com>
2023-03-30 09:29:26 +0100

    Fix Gantt issue when step titles have a colon (#36)

--- src/stepTracer.ts
+++ src/stepTracer.ts
@@ -29,7 +29,10 @@ function generateTraceChartForSteps(job: WorkflowJobType): string {
     if (!step.started_at || !step.completed_at) {
       continue
     }
-    chartContent = chartContent.concat('\t', `${step.name} : `)
+    chartContent = chartContent.concat(
+      '\t',
+      `${step.name.replace(/:/g, '-')} : `
+    )
 
     if (step.name === 'Set up job' && step.number === 1) {
       chartContent = chartContent.concat('milestone, ')
